7e862016d1a7ae93dc0154acd3894706729c04fd
Matt Graham <matthew.m.graham@gmail.com>
2017-06-01 03:29:22 +0100

    Updating chapter compilation script to support bibtex.

--- compile-chapter.sh
+++ compile-chapter.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 pdflatex -jobname=chapter-$1 "\includeonly{chapters/chapter-$1}\input{thesis}"
+bibtex chapter-$1.aux
+pdflatex -jobname=chapter-$1 "\includeonly{chapters/chapter-$1}\input{thesis}"
+pdflatex -jobname=chapter-$1 "\includeonly{chapters/chapter-$1}\input{thesis}"
